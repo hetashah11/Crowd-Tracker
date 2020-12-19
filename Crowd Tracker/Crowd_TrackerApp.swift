2b791a35de5427743d1df13516ec0aa8ac717919
Dhruv Vikram Krishna <dhruv.vikram.krishna@gmail.com>
2020-12-19 17:16:20 +0530

    Initial Commit

---
+++ Crowd Tracker/Crowd_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crowd_TrackerApp.swift
+//  Crowd Tracker
+//
+//  Created by Dhruv Vikram Krishna on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Crowd_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
